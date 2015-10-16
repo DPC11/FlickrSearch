0d0e8ab507a1689ac1c516df1eab35163e25e796
DPC <dpc11@qq.com>
2015-10-16 18:02:31 +0800

    Part 1 of this practice

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by DPC on 15/10/16.
+//  Copyright © 2015年 DPC. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell
+{
+    @IBOutlet weak var imageView: UIImageView!    
+}
